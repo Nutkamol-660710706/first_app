0547a684890b130fe8a7d66e188f667f6e32cf18
Nutkamol-660710706 <piriyatanasrub_n@su.ac.th>
2025-08-09 15:00:41 +0700

    first commit app v1

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerLeft,
+          child:Text('Layout',style:TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
+        ),backgroundColor: Colors.blue,
+      ),
+      body:Center(child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Container(height: 300, width: 120,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle ,color: Colors.black , 
+              borderRadius: BorderRadius.circular(20))),
+          Positioned(
+            top: 20,
+            child: 
+              Container(height: 80, width: 80 ,
+              decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red))
+            ),
+          Positioned(
+            child: 
+              Container(height: 80, width: 80 ,
+              decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.yellow))
+            ),
+          Positioned(
+            bottom: 20,
+            child: 
+              Container(height: 80, width: 80 ,
+              decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.green))
+            ),
+        ],
+      ),) 
+    ); 
+  }
+}
